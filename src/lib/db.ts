0a08b4bbaa4fbc38ce7c3851e744b44341fcef39
Christopher Vos <christophervos522@gmail.com>
2026-01-05 23:14:27 +0200

    added the table types

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -12,3 +12,34 @@ export function createClientConnection(): boolean {
     return false;
   }
 }
+export interface MetaData {
+  id: Number,
+  created_at: Date
+}
+
+export interface Ticket {
+  fullName: string,
+  email: string,
+  phoneNumber: string,
+  status: "pending" | "active" | "complete" | "canceled"
+}
+
+export interface Event {
+  ticket_id: Number,
+  type: string,
+  date: Date,
+  number_of_guests: Number,
+  number_of_chairs: Number,
+  number_of_tables: Number,
+  catering: boolean,
+  services?: string[],
+  requests?: string
+}
+
+const tableNames: Record<string, string> = {
+  'data': 'event-data',
+  'tickets': "event-tickets"
+};
+
+
+// TODO: make a function to subscribe to the realtime database
