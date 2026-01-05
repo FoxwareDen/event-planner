7e46bffa34c773dd96fa7b8f5341d24e6bf5d7e2
Christopher Vos <christophervos522@gmail.com>
2026-01-05 15:49:38 +0200

    added db instance check

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,6 +4,7 @@ export let db: null | SupabaseClient = null
 
 export function createClientConnection(): boolean {
   try {
+    if (db) return true;
     db = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
     return true;
   } catch (error) {
